5cb868b420318a14c0df7d4a86d83d4583f67767
ohteki <ohteki@syun.jp>
2018-04-17 19:10:53 +0900

    set arguments

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 //% weight=70 icon="\u30A2" color=#EC7505 block="超音波距離センサ"
 namespace uds {
 
-    //% blockId=uds block="距離を測定"
-    export function get(): void {
+    //% blockId=uds block="距離を測定 trig %trig|echo %echo"
+    export function get(trig: DigitalPin, echo: DigitalPin): number {
       let distance = 0
       pins.digitalWritePin(DigitalPin.P0, 0)
       control.waitMicros(2)
@@ -10,11 +10,6 @@ namespace uds {
       control.waitMicros(20)
       pins.digitalWritePin(DigitalPin.P0, 0)
       distance = pins.pulseIn(DigitalPin.P1, PulseValue.High) * 153 / 29 / 2 / 100
-      if (distance < 20) {
-          basic.showIcon(IconNames.Happy)
-      } else {
-          basic.showIcon(IconNames.Sad)
-      }
-      basic.pause(10)
+      return distance
     }
 }
