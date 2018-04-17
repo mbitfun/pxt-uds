7cd0063158bbe8ded2e4eb68ffe7824aba29dc11
ohteki <ohteki@syun.jp>
2018-04-17 19:19:57 +0900

    set arguments

--- main.ts
+++ main.ts
@@ -3,13 +3,11 @@ namespace uds {
 
     //% blockId=uds block="距離を測定 trig %trig|echo %echo"
     export function get(trig: DigitalPin, echo: DigitalPin): number {
-      let distance = 0
-      pins.digitalWritePin(DigitalPin.P0, 0)
+      pins.digitalWritePin(trig, 0)
       control.waitMicros(2)
-      pins.digitalWritePin(DigitalPin.P0, 1)
+      pins.digitalWritePin(trig, 1)
       control.waitMicros(20)
-      pins.digitalWritePin(DigitalPin.P0, 0)
-      distance = pins.pulseIn(DigitalPin.P1, PulseValue.High) * 153 / 29 / 2 / 100
-      return distance
+      pins.digitalWritePin(trig, 0)
+      return pins.pulseIn(echo, PulseValue.High) * 153 / 29 / 2 / 100
     }
 }
