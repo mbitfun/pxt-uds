613d8757e5ce1f33007ab35ad794c67d7fc5b446
ohteki <ohteki@syun.jp>
2018-04-17 18:45:08 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # pxt-uds
+
+## License
+MIT
+
+## Supported targets
+
+* for PXT/microbit
---
+++ main.ts
@@ -0,0 +1,20 @@
+//% weight=70 icon="\u30A2" color=#EC7505 block="超音波距離センサ"
+namespace uds {
+
+    //% blockId=uds block="距離を測定"
+    export function get(text: string, time: number = 500): void {
+      let distance = 0
+      pins.digitalWritePin(DigitalPin.P0, 0)
+      control.waitMicros(2)
+      pins.digitalWritePin(DigitalPin.P0, 1)
+      control.waitMicros(20)
+      pins.digitalWritePin(DigitalPin.P0, 0)
+      distance = pins.pulseIn(DigitalPin.P1, PulseValue.High) * 153 / 29 / 2 / 100
+      if (distance < 20) {
+          basic.showIcon(IconNames.Happy)
+      } else {
+          basic.showIcon(IconNames.Sad)
+      }
+      basic.pause(10)
+    }
+}
